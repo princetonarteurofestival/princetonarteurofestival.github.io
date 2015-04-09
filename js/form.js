a8237b28334906207b252065a8644338bdd9877b
Sam Liu <ontouchstart@gmail.com>
2015-04-09 21:49:48 +0000

    add reviewModal

--- js/form.js
+++ js/form.js
@@ -17,5 +17,29 @@ $(document).ready(function() {
             {% include instrument.js %},
             {% include address.js %}
         }
-    });
+    })
+    .on('success.form.bv', function(e) {
+            // Prevent form submission
+            e.preventDefault();
+            // Get the form instance
+            var $form = $(e.target);
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+            var html = '<dl>';
+            var subject = 'Courchevel International Music Festival Registration Form';
+            $.each($('#defaultForm').serializeArray(), function (k, v) { 
+              if(v.name === 'name') {
+                 subject += ' for ' + v.value;
+              }
+              if(v.value) {
+                html += '<dt>' + v.name + '</dt><dd>' + v.value + '</dd>' ;
+              }
+            });
+            html += '</dl>';
+            $('#review').html(html);
+            $('#reviewButton').click();
+            $('#submit').click(function () {
+              document.location.href = '/';
+            });
+        });
 });
